e2d0e537e26993209cc80908a6b147234d6ebf61
Mikhail Sklyar <work@miksklyar.ru>
2026-03-18 20:00:34 +0700

    add edit text and open SecondActivity with it

--- main/src/main/java/ci/nsu/mobile/main/ui/main/MainFragment.kt
+++ main/src/main/java/ci/nsu/mobile/main/ui/main/MainFragment.kt
@@ -1,13 +1,17 @@
 package ci.nsu.mobile.main.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import ci.nsu.mobile.main.R
+import ci.nsu.mobile.main.SecondActivity
 
 class MainFragment : Fragment() {
 
@@ -26,10 +30,29 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val editText = view.findViewById<EditText>(R.id.edit_text_text)
         val button = view.findViewById<Button>(R.id.button_open_second)
+
         button.setOnClickListener {
-            viewModel.onButtonClicked()
+            viewModel.onButtonClicked(editText.text.toString())
+        }
+
+        viewModel.openSecondActivityEvent.observe(viewLifecycleOwner) { text ->
+            text?.let { validText ->
+                openSecondActivity(validText)
+            }
         }
     }
 
+    fun showError() {
+        Toast.makeText(requireContext(), "Поле не должно быть пустым!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun openSecondActivity(text: String) {
+        val context = requireContext()
+
+        val intent = Intent(context, SecondActivity::class.java)
+        intent.putExtra("EXTRA_TEXT", text)
+        context.startActivity(intent)
+    }
 }
--- main/src/main/java/ci/nsu/mobile/main/ui/main/MainViewModel.kt
+++ main/src/main/java/ci/nsu/mobile/main/ui/main/MainViewModel.kt
@@ -1,11 +1,21 @@
 package ci.nsu.mobile.main.ui.main
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
-    fun onButtonClicked() {
+    private val _openSecondActivityEvent = MutableLiveData<String?>()
+    val openSecondActivityEvent: LiveData<String?> = _openSecondActivityEvent
+
+    fun onButtonClicked(inputText: String = "") {
         Log.d("Lab3", "Button clicked")
+
+        val trimmedText = inputText.trim()
+
+        // TODO: Some logic here
+        _openSecondActivityEvent.value = trimmedText.ifEmpty { null }
     }
 }
