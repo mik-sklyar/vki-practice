30f20c80b2292ef6c656c18aad68f9a470f9481f
SklyarMM <work@miksklyar.ru>
2026-03-19 16:26:36 +0700

    add okhttp lib

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -49,6 +49,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // 3d party
+    implementation(libs.okhttp)
+
+    // Tests and debug
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
